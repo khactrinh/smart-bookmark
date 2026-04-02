0e6f00cb54cd1dabbd4fa88be5f66b798b670d18
khactrinh <khactrinhcn02@gmail.com>
2026-04-02 21:07:52 +0700

    fix bug refresh

--- app/Providers.jsx
+++ app/Providers.jsx
@@ -2,5 +2,5 @@
 import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider refetchOnWindowFocus={false}>{children}</SessionProvider>;
 }
